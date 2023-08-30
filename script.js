2588861c19460128bfec773543d2e1fe70b6b4a9
chendodev <chendoec@live.com>
2023-08-30 11:16:22 -0500

    Calculator Project - HTML & CSS

---
+++ script.js
@@ -0,0 +1,17 @@
+const othersOp = document.getElementById("othersOp");
+const numbers = document.getElementById("numbers");
+
+const OTHERS_OP = [
+  "X", "Calc", "", "", "Pi", "x³", "ab", 
+  "√", "x²", "^", "log", "In", 
+  "(-)", ".,,,", "hyp", "sin", "cos", 
+  "tan", "Rc1", "Eng", "(",
+  ")", ",", "m+"
+]
+const NUMBERS = [
+  "7", "8", "9", "DEL", "AC", 
+  "4", "5", "6", "*", "/", 
+  "1", "2", "3", "+", "-", 
+  "0", ".", "EXP", "Ans",
+  "="
+]
